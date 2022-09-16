2c7a961d5e17389d054ce1c7f11d0c4adb1344b3
Sanshiroh SAKAI <sansyo@gmail.com>
2022-09-17 06:42:29 +0900

    Standard Style および eslint-plugin-react と @typescript-eslint/eslint-plugin の設定を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,24 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react'],
   rules: {
     // 静的サイト構築にともなう img タグの記述許可
     '@next/next/no-img-element': 'off',
   },
+  overrides: [],
 }
