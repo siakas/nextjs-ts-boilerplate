1471fb3875d4642970040aa157786cea0493c8b4
Sanshiroh SAKAI <sansyo@gmail.com>
2022-09-18 05:27:49 +0900

    update stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
     'stylelint-config-css-modules',
     'stylelint-config-recess-order',
     'stylelint-config-prettier',
