20ddff31b8421aff689ff017a823baa1ad0427cc
Sanshiroh SAKAI <sansyo@gmail.com>
2022-09-17 10:35:03 +0900

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,21 @@ module.exports = {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
 
+    /* boolean 変数の受け渡しには JSX の省略形を使う */
+    'react/jsx-boolean-value': 'error',
+
+    /* 文字列属性に波括弧は不要 */
+    'react/jsx-curly-brace-presence': 'error',
+
+    /* 子要素のないコンポーネントは自己終了タグを使う（HTML タグは除外） */
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: false,
+      },
+    ],
+
     /* 静的サイト構築にともなう img タグの記述許可 */
     '@next/next/no-img-element': 'off',
 
@@ -40,5 +55,13 @@ module.exports = {
       },
     ],
   },
-  overrides: [],
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        /* TypeScript では PropTypes による型チェックは不要 */
+        'react/prop-types': 'off',
+      },
+    },
+  ],
 }
